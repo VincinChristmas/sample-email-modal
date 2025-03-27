b1382b71190fe887438e71d48216838ab25321ba
VincinChristmas <VincinChristmas@hotmail.com>
2025-03-27 17:17:42 -0500

    worked on the JavaScript for the email

---
+++ js/app.js
@@ -0,0 +1,28 @@
+windows.onload = function() {
+    let emailState = false;
+    let emailModal = document.getElementsByClassName("email-modal")[0];
+    let closeButton = document.getElementsByClassName( "email-modal__close-btn")[0];
+    let emailInput = document.getElementsByClassName("email-modal__input")[0];
+    let emailButton = document.getElementsByClassName("email-modal__button")[0];
+    let thankContainer = document.getElementsByClassName("email-thank")[0];
+    let declineOffer = document.getElementsByClassName("email-modal__decline-offer" )[0];
+
+    function emailValid(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
+    let showModal = () => {
+        if (emailState === false) {
+            emailModal.classList.add("email-modal--visible");
+            emailState = true;
+        }
+    }
+
+    let closeModal = () {
+        emailModal.classList.remove("email-modal--visable");
+    }
+
+
+
+
+}
